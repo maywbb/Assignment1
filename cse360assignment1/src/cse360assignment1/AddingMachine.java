06b3c2311f6f1709a72bf4bf5d5132d49f241aa8
Mayumi Webb <vampsc13@gmail.com>
2020-10-06 12:59:26 -0700

    AddingMachine with empty methods

---
+++ cse360assignment1/src/cse360assignment1/AddingMachine.java
@@ -0,0 +1,63 @@
+package cse360assignment1;
+
+/**
+ * Class contains a constructor to create an adding machine that adds and subtracts
+ * As well, it retrieves the total and places the transaction into String format
+ * @author MelonPop
+ */
+public class AddingMachine 
+{
+	private int total;
+
+	/**
+	 * Constructor to create an Adding Machine object
+	 * @return new AddingMachin() object
+	 */
+	public AddingMachine()
+	{
+		total = 0;
+	}
+	/**
+	 * returns the current total
+	 * @return the total
+	 */
+	public int getTotal()
+	{
+		return 0;
+	}
+	
+	/**
+	 * adds the parameter value to the total
+	 * @param value
+	 */
+	public void add(int value)
+	{
+		
+	}
+	/**
+	 * subtracts parameter value from total
+	 * @param value
+	 */
+	public void subtract(int value)
+	{
+		
+	}
+	
+	/**
+	 * places the entire transaction that occurs in String format
+	 * @return entire transaction
+	 */
+	public String toString()
+	{
+		return "";
+	}
+	
+	/**
+	 * clears the memory (all the transactions that took place)
+	 * @param no parameter
+	 */
+	public void clear()
+	{
+		
+	}
+}
